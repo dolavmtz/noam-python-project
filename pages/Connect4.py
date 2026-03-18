15f83e3509d86f5e4c06fd49fc6ab340d8d40064
dolavmtz <dolavmtz88@gmail.com>
2026-03-18 18:45:56 +0200

    המשכנו 4 בשורה

--- pages/Connect4.py
+++ pages/Connect4.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import random
 
 #משתנים קבועים
 ROWS = 6
@@ -8,6 +8,10 @@ COLS = 7
 PLAYER = "🟡"
 COMPUTER = "⚫"
 EMPTY = "⚪"
+
+def computerTurn():
+    randomCol = random.randint(0,COLS-1)
+    click(randomCol)
 
 #לוח
 def newBoard():
@@ -19,10 +23,34 @@ def newBoard():
         board.append(row)
     return board
 
-board = newBoard()
+if "board" not in st.session_state:
+    st.session_state.board = newBoard()
+
+board = st.session_state.board
+
+if "turn" not in st.session_state:
+    st.session_state.turn = PLAYER
+
+turn = st.session_state.turn
+
+def switchTurn():
+    if st.session_state.turn == PLAYER:
+        st.session_state.turn = COMPUTER
+    else:
+        st.session_state.turn = PLAYER
 
 def click(col):
-    columns = st.columns(COLS)
+    #st.write(col)
+    for i in range(ROWS - 1,-1, -1):
+        if board[i][col]==EMPTY:
+            board[i][col] = turn
+            break
+    st.session_state.board = board
+    switchTurn()
+    st.rerun()
+
+if turn == COMPUTER:
+    computerTurn()
 
 for r in range(ROWS):
     columns = st.columns(COLS)
