a8fd42a7bbd58bdf65acad1f7a77ebfc6a04a123
dolavmtz <dolavmtz88@gmail.com>
2026-04-29 16:47:35 +0300

    המשכנו 4 בשורה

--- pages/Connect4.py
+++ pages/Connect4.py
@@ -42,18 +42,33 @@ def switchTurn():
         st.session_state.turn = PLAYER
 
 def checkWinner(check_row, check_col):
-    for row in range(ROWS):
-        for cell in range(COLS - 3):
-            if board[row][cell] == EMPTY:
-                continue
-            for i in range(cell,cell+4):
-                if board[row][i] != board[row][cell]:
-                    print(f"No Win starts with row {row} cell {cell}")
-                    break
-            else:
-                print("הגיעה לסיום")
-                print(board[row][cell])
-                return board[row][cell]
+    #for row in range(ROWS):
+    row = check_row
+    for cell in range(COLS - 3):
+        if board[row][cell] == EMPTY:
+            continue
+        for i in range(cell,cell+4):
+            if board[row][i] != board[row][cell]:
+                print(f"No Win starts with row {row} cell {cell}")
+                break
+        else:
+            print("הגיעה לסיום")
+            print(board[row][cell])
+            return board[row][cell]
+
+    col = check_col
+    for cell in range(ROWS - 3):
+        if board[cell][col] == EMPTY:
+            continue
+        for i in range(cell,cell+4):
+            if board[i][col] != board[cell][col]:
+                print(f"No Win starts with row {row} cell {cell}")
+                break
+        else:
+            print("הגיעה לסיום")
+            print(board[cell][col])
+            return board[cell][col]
+
 def click(col):
     if board[0][col] != EMPTY:
         return
@@ -62,24 +77,36 @@ def click(col):
     for i in range(ROWS - 1,-1, -1):
         if board[i][col]==EMPTY:
             board[i][col] = turn
-            checkWinner(i,col)
+            st.session_state.winner = checkWinner(i,col)
             break
     st.session_state.board = board
     switchTurn()
     st.rerun()
 
-if turn == PLAYER:
-    st.info("עכשיו תור השחקן")
+winner =None
+if "winner" in st.session_state:
+    winner = st.session_state.winner
+
+can_play = True
+if winner == PLAYER:
+    st.info("ניצחת!")
+    can_play = False
+elif winner == COMPUTER:
+    st.info("המחשב ניצח")
+    can_play = False
 else:
-    st.status("המחשב חושב")
+    if turn == PLAYER:
+        st.info("עכשיו תור השחקן")
+    else:
+        st.status("המחשב חושב")
 
 for r in range(ROWS):
     columns = st.columns(COLS)
     for c in range(COLS):
         with columns[c]:
             cell = board[r][c]
-            if st.button(cell,key= f"row_{r}_col_{c}",use_container_width=True, disabled= turn==COMPUTER):
+            if st.button(cell,key= f"row_{r}_col_{c}",use_container_width=True, disabled= turn==COMPUTER or not can_play):
                 click(c)
 
-if turn == COMPUTER:
+if turn == COMPUTER and can_play:
     computerTurn()
